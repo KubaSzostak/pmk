b299196e52270bb17311b1ba649dea8a3278c528
Kuba Szostak <kuba.szostak@gmail.com>
2014-03-23 15:11:05 +0100

    initial gh-pages for PMK

---
+++ pmk/geosoft.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+//--------------------------------------------------------------------------------------------
+function OpenDlgWindow(url, width, height) {
+  window.open(url, 'displayWindow','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=yes,copyhistory=no,width= ' + (width) + ',height=' + (height));
+}
+//--------------------------------------------------------------------------------------------
